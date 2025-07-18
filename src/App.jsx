8136cd16327214bb35b33ab126d0fec10a19ad00
lavanyaadhi2024@gmail.com <lavanyaadhi2024@gmail.com>
2025-07-18 10:28:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+import React from 'react';
+import Parallax from './component/Parallax'; // or './components/Message' if you used a folder
+
+function App() {
+  return (
+    <div>
+      <Parallax />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Parallax.jsx
@@ -0,0 +1,43 @@
+// src/Message.jsx
+
+import React from 'react';
+import './Parallax.css';
+const Parallax = ()=> {
+  return (
+    <div>
+        <div className="parallax">
+            <section className="center"><h1>LOvE of the WORLD</h1></section>
+        </div>
+        <section className="content">
+            <h1>Welcome to the world of Universe</h1>
+            <p>Our universe is vey big...Being a very very small minute living being of this universe, I am doing my first website using HTML CSS and JS about our WORLD with NATURE....</p>
+        </section>
+  
+
+        <div className="parallax2"></div>
+
+            <section className="content">
+                <h2>Beauty of Forest</h2>
+                <p>Forests are large areas dominated by trees and other vegetation, playing a vital role in regulating climate, supporting biodiversity, and providing resources for humans and animals. They act as vital ecosystems, contributing to air and water purification, and providing habitats for countless species. .</p>
+             </section>
+
+        <div className="parallax3"></div>
+            <section className="content">
+                <h2>Beauty of Nature</h2>
+                <p> Many beautiful living things are among the countless riches of beauty that nature has to offer. Millions of different species in every size, colour, and habitat—on land, in the sky, and in the water—abound in the world of birds, animals, reptiles, and fish. They are present all the time and everywhere.</p>
+            </section>
+
+        <div className="parallax4"></div>
+            <section className="content">
+                <h2>Beauty of HillStation</h2>
+                <p>Hill stations are renowned for their natural beauty, offering stunning landscapes, cool climates, and a peaceful escape from the hustle and bustle of city life. They often feature lush green hills, vibrant flora, and panoramic views, making them a popular destination for relaxation and rejuvenation. </p>
+            </section>
+
+            <div className="parallax5"></div>
+            <section className="center-text">End of the page</section>
+        
+    </div>
+  );
+};
+
+export default Parallax;
